b51572598c2304b93f1a7401399d8cf7797aaf84
saran sinha <saransinha1522@gmail.com>
2022-12-27 21:15:21 +0530

    modified files

--- script.js
+++ script.js
@@ -1,23 +1,62 @@
-let characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
-
 let boxEl_1=document.getElementById("box1")
 let boxEl_2=document.getElementById("box2")
-let inpEl=0;
+let inpEl=0
 let password1=""
 let password2=""
 
+const upperStr ="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const lowerStr="abcdefghijklmnopqrstuvwxyz"
+const numsStr="1234567890"
+const symbolsStr="`~!@#$%^&*()_-+=[]\{}|;':,./<>?"
+
+const getLower=()=>{
+    const ind= Math.floor(Math.random()*lowerStr.length);
+    return lowerStr[ind]
+}
+const getUpper=()=>{
+    const ind= Math.floor(Math.random()*upperStr.length);
+    return upperStr[ind]
+}
+const getNums=()=>{
+    const ind= Math.floor(Math.random()*numsStr.length);
+    return numsStr[ind]
+}
+const getSymbol=()=>{
+    const ind= Math.floor(Math.random()*symbolsStr.length);
+    return symbolsStr[ind]
+}
+
 function generate(){
-    inpEl=document.getElementById("inp-el").value;
-    for (let i = 1; i <= inpEl; i++) {
-        let random1=Math.floor(Math.random()*characters.length)
-        let random2=Math.floor(Math.random()*characters.length)
-        
-        password1+= characters[random1]
-        password2+= characters[random2]
+    inpEl=+document.getElementById("inp-el").value;
+
+    const hasLower=document.getElementById('lower-check').checked
+    const hasUpper=document.getElementById('upper-check').checked
+    const hasNums=document.getElementById('nums-check').checked
+    const hasSymbol=document.getElementById('symbol-check').checked
+
+    for (let i = 1; i <= inpEl; ++i) {
+        if(hasLower)
+            password1+=getLower()
+        if(hasUpper)
+            password1+=getUpper()
+        if(hasNums)
+            password1+=getNums()
+        if(hasSymbol)
+            password1+=getSymbol()
     }
-    boxEl_1.textContent=password1
-    boxEl_2.textContent=password2
+
+    if(inpEl===0)
+    {
+        boxEl_1.textContent=" "
+        boxEl_2.textContent=" "
+    }
+    else
+    {
+        password1=password1.slice(0,inpEl)
+        boxEl_1.textContent=password1
+        password2=password1.split("").reverse().join("");
+        boxEl_2.textContent=password2
+    }
     password1=""
     password2=""
 }
